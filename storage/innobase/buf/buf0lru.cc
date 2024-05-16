88c4ee57da6983af51b507e20112d3a743da582f
mariadb-DebarunBanerjee <debarun.banerjee@mariadb.com>
2024-05-16 13:10:44 +0530

    MDEV-34167 We fail to terminate transaction early with ER_LOCK_TABLE_FULL when lock memory is growing
    This regression is introduced in 10.6 by following commit.
    commit b6a2472489accf0ae9ac3655ffe9b2997ab267ba
    MDEV-27891: SIGSEGV in InnoDB buffer pool resize
    
    During DML, we check if buffer pool is running out of data pages in
    buf_pool_t::running_out. Here is 75% of the buffer pool is occupied by
    non-data pages we rollback the current transaction and exit with
    ER_LOCK_TABLE_FULL.
    
    The integer division (n_chunks_new / 4) becomes zero whenever the total
    number of chunks are < 4 making the check completely ineffective for
    such cases. Also the check is inaccurate for larger chunks.
    
    Fix-1: Correct the check in buf_pool_t::running_out.
    
    Fix-2: While waiting for free page, check for
    buf_LRU_check_size_of_non_data_objects.

--- storage/innobase/buf/buf0lru.cc
+++ storage/innobase/buf/buf0lru.cc
@@ -450,8 +450,10 @@ got_block:
       buf_pool.page_cleaner_wakeup(true);
     mysql_mutex_unlock(&buf_pool.flush_list_mutex);
     if (my_cond_timedwait(&buf_pool.done_free, &buf_pool.mutex.m_mutex,
-                          &abstime))
+                          &abstime)) {
       buf_pool.LRU_warn();
+      buf_LRU_check_size_of_non_data_objects();
+    }
   }
 
   goto got_block;
